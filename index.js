c3d222fe7051fe0b0affd12dfe1a11b982ddda80
fyrodems <fyrodems@gmail.com>
2022-06-04 23:11:48 +0200

    change color while speaking

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-import { speedInput, sppedValue } from './constants';
-import { speak } from './constants';
-// work_pls :(((((((((((((((((((((((((((((((((((((((((((((;
-
-console.log(speedInput, sppedValue)
-console.log(speak)
-
-
-
 const spk = window.speechSynthesis;
 console.log(spk)
 
 console.log(spk.getVoices())
+
 
 $('#play').on('click', () => {
     const txt = $('textarea').val();
@@ -19,6 +11,10 @@ $('#play').on('click', () => {
     txtToRead.volume = $('#volume').val();
     txtToRead.pitch = $('#pitch').val();
     txtToRead.rate = Number($('#speed').val());
+
+    txtToRead.onend = event => {
+        $('.logo').css('color', 'red')
+     }
 
     console.log(txt)
     console.log(txtToRead)
@@ -29,6 +25,8 @@ $('#play').on('click', () => {
         $('.logo').css('color', 'green')
     }
 })
+
+
 
 $('#pause').on('click', () => {
 
